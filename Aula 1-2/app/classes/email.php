fec392cf6bd172ac60a7133e674bd9aef1f4a3a2
Aparecido Nicolini <you@example.com>
2023-12-17 13:15:24 -0300

    Atualização Curso PHP Orientado a objetos

--- Aula 1-2/app/classes/email.php
+++ Aula 1-2/app/classes/email.php
@@ -1,9 +1,16 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\PHPMailer;
+namespace app\classes;
 
-class Email extends PHPMailer {
+// class Email extends PHPMailer {
+//     public function send() {
+//         return 'Enviar email';
+//     }
+// }
+
+class Email {
     public function send() {
-        return 'Enviar email';
+        return 'Enviar email 1';
     }
 }
--- Aula 1-2/index.php
+++ Aula 1-2/index.php
@@ -1,9 +1,42 @@
 <?php
 
-    require 'vendor/autoload.php';
-    require 'app/classes/Email.php';
+use app\classes\Email;
 
-    $email = new Email;
-    echo $email->send();
+    spl_autoload_register(function ($class) {
+
+        // project-specific namespace prefix
+        $prefix = 'app\\';
+        // $prefix é como vai começar o namespace 
+    
+        // base directory for the namespace prefix
+        $base_dir = __DIR__ . '/app/';
+        // $base_dir é da onde será enviado as classes
+    
+        // does the class use the namespace prefix?
+        $len = strlen($prefix);
+        if (strncmp($prefix, $class, $len) !== 0) {
+            // no, move to the next registered autoloader
+            return;
+        }
+    
+        // get the relative class name
+        $relative_class = substr($class, $len);
+    
+        // replace the namespace prefix with the base directory, replace namespace
+        // separators with directory separators in the relative class name, append
+        // with .php
+        $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    
+        // if the file exists, require it
+        if (file_exists($file)) {
+            require $file;
+        }
+    });
+
+    // $email = new app\classes\Email;
+    // echo $email->send();
+
+    $produto = new app\models\Produto;
+    echo $produto->create();
 
 ?>
